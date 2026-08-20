b4df616606b79d03cff02757e6037f2e789e5b53
yueyinqiu <yueyinqiu@outlook.com>
2026-08-20 12:28:34 +0800

    auto gc

--- src/parts/nix/default.nix
+++ src/parts/nix/default.nix
@@ -2,6 +2,11 @@
   ...
 }:
 {
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 7d";
+  };
   nix.settings.experimental-features = [
     "nix-command"
     "flakes"
