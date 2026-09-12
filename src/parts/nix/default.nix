388449e3e3fb12db8c4b5f3c4fd27c4d37ad0915
yueyinqiu <yueyinqiu@outlook.com>
2026-09-12 11:21:21 +0800

    better clean

--- src/parts/nix/default.nix
+++ src/parts/nix/default.nix
@@ -1,9 +1,5 @@
+
 {...}: {
-  nix.gc = {
-    automatic = true;
-    dates = "weekly";
-    options = "--delete-older-than 7d";
-  };
   nix.settings.experimental-features = [
     "nix-command"
     "flakes"
