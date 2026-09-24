4d191093ecc2374036840545f674972dff12e3ad
yueyinqiu <yueyinqiu@outlook.com>
2026-09-24 16:03:39 +0800

    add more subsitituters

--- src/parts/nix/default.nix
+++ src/parts/nix/default.nix
@@ -17,12 +17,18 @@
 
   nix.settings.extra-substituters = [
     "https://yueyinqiu.cachix.org"
+    "https://nix-community.cachix.org"
+    "https://devenv.cachix.org"
     "https://nvf.cachix.org"
-    "https://doom-emacs-unstraightened.cachix.org/"
+    "https://niri.cachix.org"
+    "https://numtide.cachix.org"
   ];
   nix.settings.extra-trusted-public-keys = [
     "yueyinqiu.cachix.org-1:iooLFYpS7e6KAU4+QM5Zoj6Tq76jRGo+kjeAbu8JxAc="
+    "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
     "nvf.cachix.org-1:GMQWiUhZ6ux9D5CvFFMwnc2nFrUHTeGaXRlVBXo+naI="
-    "doom-emacs-unstraightened.cachix.org-1:O5oOlRPnmQEvVaFyuMTmthCEooHbrg54WgSLR07tmg4="
+    "niri.cachix.org-1:Wv0OmO7PsuocRKzfDoJ3mulSl7Z6oezYhGhR+3W2964="
+    "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
   ];
 }
